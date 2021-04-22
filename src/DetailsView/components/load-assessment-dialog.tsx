1122047eee959694827b3e90f28012abcf17ba96
shanisebarona <shanisebarona@gmail.com>
2021-04-22 18:18:46 -0400

    refacotr target change dialog into a parent component and two composed from it

---
+++ src/DetailsView/components/load-assessment-dialog.tsx
@@ -0,0 +1,47 @@
+import { Tab } from 'common/itab';
+import { NamedFC } from 'common/react/named-fc';
+import { PersistedTabInfo } from 'common/types/store-data/assessment-result-data';
+import { UrlParser } from 'common/url-parser';
+import {
+    ChangeAssessmentDialog,
+    ChangeAssessmentDialogProps,
+} from 'DetailsView/components/change-assessment-dialog';
+
+import * as React from 'react';
+import { DetailsViewActionMessageCreator } from '../actions/details-view-action-message-creator';
+
+export type LoadAssessmentDialogDeps = {
+    urlParser: UrlParser;
+    detailsViewActionMessageCreator: DetailsViewActionMessageCreator;
+};
+
+export interface LoadAssessmentDialogProps {
+    deps: LoadAssessmentDialogDeps;
+    prevTab: PersistedTabInfo;
+    newTab: Tab;
+}
+
+export const LoadAssessmentDialog = NamedFC<LoadAssessmentDialogProps>('LoadAssessment', props => {
+    const dialogProps: ChangeAssessmentDialogProps = {
+        deps: props.deps,
+        prevTab: props.prevTab,
+        newTab: props.newTab,
+        dialogContentTitle: 'Assessment in progress',
+        subtitleAriaId: 'load-assessment-dialog-description',
+        divId: "load-assessment-dialog-description",
+        leftButtonText: "Continue previous",
+        leftButtonOnClick: props.deps.detailsViewActionMessageCreator
+        .continuePreviousAssessment,
+        rightButtonText: "Load Assessment",
+        rightButtonOnClick: props.deps.detailsViewActionMessageCreator
+        .startOverAllAssessments, //change to load assessment
+        dialogFirstText: <>Would you like to continue your current assessment or load the new Assessment?</>,
+        dialogNoteText: "If 'Continue previous' is selected, the assessment selected will not be loaded.",
+        dialogWarningText: "If 'Load assessment’ is selected, all previous progress will be lost.",
+    };
+
+    return (
+        <ChangeAssessmentDialog {...dialogProps}
+        />
+    );
+});
